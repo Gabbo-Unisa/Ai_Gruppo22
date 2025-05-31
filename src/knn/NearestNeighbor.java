2fd9ac904a9c85cd62f6884402f3fe39d67d069c
Pasquale Piccolo <p.piccolo5@studenti.unisa.it>
2025-05-31 13:21:56 +0200

    Creazione classi per KNN e implementazione KDTree.java

---
+++ src/knn/NearestNeighbor.java
@@ -0,0 +1,30 @@
+package knn;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NearestNeighbor {
+    private List<Sample>  trainingData;
+    private KDTree kdtree;
+    private int[] classCounts;
+    private double[] featureMin;
+    private double[] featureMax;
+    private String firstLineOfTheFile;
+
+    public NearestNeighbor(String filename) {
+
+    }
+
+    private void readAndNormaliza(String filename) {
+
+    }
+
+    private void readPointFromCSV(String filename) {
+
+    }
+
+
+}
