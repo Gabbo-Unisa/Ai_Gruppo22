f7b9a24d65f0dc92d71178feb5ee0dbac017497c
Gabbo <g.pannuto@studenti.unisa.it>
2025-06-03 13:23:12 +0200

    Acconciato le marce che sul fuori pista aveva problemi

--- src/scr/AcquisisciDriver.java
+++ src/scr/AcquisisciDriver.java
@@ -17,8 +17,8 @@ public class AcquisisciDriver extends Controller {
     private boolean steerLilRight;
 
     /* Costanti di cambio marcia */
-    final int[] gearUp = { 11000, 14000, 18000, 18000, 18000, 18000, 0 };
-    final int[] gearDown = { 0, 5000, 5000, 12000, 12000, 12000, 12000 };
+    final int[] gearUp = { 16000, 18000, 18000, 18000, 18000, 18000, 0 };
+    final int[] gearDown = { 0, 9000, 9000, 12000, 12000, 12000, 12000 };
 
     /* Costanti */
     final int stuckTime = 25;
@@ -59,7 +59,7 @@ public class AcquisisciDriver extends Controller {
     public AcquisisciDriver() {
         SwingUtilities.invokeLater(() -> new ContinuousCharReaderUI(this));
         try {
-            csvWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream("driving_data%4f.csv")));
+            csvWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream("incidente curva 1-1.csv")));
             csvWriter.append("angle;curLapTime;damage;distRaced;distFromStart;speedX;speedY;track_0;track_1;track_2;track_3;track_4;track_5;track_6;track_7;track_8;track_9;track_10;track_11;track_12;track_13;track_14;track_15;track_16;track_17;track_18;trackPos;class\n");
         } catch (IOException e) {
             e.printStackTrace();
